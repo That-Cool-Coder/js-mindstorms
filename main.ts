6859a52d4d02582f24353a29295f78262840a0a1
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-06-25 09:11:31 +0930

    wrote a program

--- main.ts
+++ main.ts
@@ -1,24 +1,47 @@
-const squareCount = 1;
+const robot = {
+    cmPerRotation : 17.6,
+    degreesPerRotation : 1 / 90,
+    motorSet : motors.largeBC
+}
 
-const degreesPerTurn = 90 / 3;
-
-function turn(degrees: number, motorSpeed: number=0) {
+function turn(degrees: number, motorSpeed: number = 50) {
     degrees = (degrees + 180) % 360 - 180;
-    let lMotorMult;
-    let rMotorMult;
     if (degrees < 0) {
-        lMotorMult = -1;
-        rMotorMult = 1;
+        robot.motorSet.tank(-motorSpeed,
+            motorSpeed, degrees * robot.degreesPerRotation,
+            MoveUnit.Rotations);
     }
     else {
-        lMotorMult = -1;
-        rMotorMult = 1;
+        robot.motorSet.tank(motorSpeed,
+            -motorSpeed, degrees * robot.degreesPerRotation,
+            MoveUnit.Rotations);
     }
-    motors.largeBC.tank(lMotorMult * motorSpeed,
-        rMotorMult * motorSpeed, degrees * degreesPerTurn,
-        MoveUnit.Rotations);
 }
 
-for (let i = 0; i < squareCount; i ++) {
-    turn(-90);
+function p_square() {
+    const squareCount = 1;
+    const squareSize = 50;
+
+    for (let i = 0; i < squareCount * 4; i++) {
+        let rotations = squareSize * robot.cmPerRotation;
+        robot.motorSet.tank(50, 50, rotations, MoveUnit.Rotations);
+        turn(-90);
+    }
 }
+
+function p_rectangle() {
+    const width = 50;
+    const height = 100;
+    const rectCount = 1;
+
+    for (let i = 0; i < rectCount * 2; i++) {
+        let rotations = width * robot.cmPerRotation;
+        robot.motorSet.tank(50, 50, rotations, MoveUnit.Rotations);
+        turn(-90);
+        rotations = height * robot.cmPerRotation;
+        robot.motorSet.tank(50, 50, rotations, MoveUnit.Rotations);
+        turn(-90);
+    }
+}
+
+square();
