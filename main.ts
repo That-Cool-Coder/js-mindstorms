49c64165fe40b0a955f13537b3cf90890c274de9
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-06-24 13:49:24 +0930

    wrote some code

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-forever(function () {
-	
-})
+const squareCount = 1;
+
+const degreesPerTurn = 90 / 3;
+
+function turn(degrees: number, motorSpeed: number=0) {
+    degrees = (degrees + 180) % 360 - 180;
+    let lMotorMult;
+    let rMotorMult;
+    if (degrees < 0) {
+        lMotorMult = -1;
+        rMotorMult = 1;
+    }
+    else {
+        lMotorMult = -1;
+        rMotorMult = 1;
+    }
+    motors.largeBC.tank(lMotorMult * motorSpeed,
+        rMotorMult * motorSpeed, degrees * degreesPerTurn,
+        MoveUnit.Rotations);
+}
+
+for (let i = 0; i < squareCount; i ++) {
+    turn(-90);
+}
